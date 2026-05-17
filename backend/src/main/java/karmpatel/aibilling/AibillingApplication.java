f71df0d89c26aebde353f2217babddb7e545e59a
Karm Patel <yourgithubemail@example.com>
2026-05-17 22:56:42 +0530

    Configured Spring Boot backend architecture

---
+++ backend/src/main/java/karmpatel/aibilling/AibillingApplication.java
@@ -0,0 +1,13 @@
+package karmpatel.aibilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AibillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AibillingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/karmpatel/aibilling/AibillingApplicationTests.java
@@ -0,0 +1,13 @@
+package karmpatel.aibilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AibillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
