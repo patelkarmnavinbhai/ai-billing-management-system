421525e99c2b8695c7172a0bd44c2f037aef4b78
Karm Patel <yourgithubemail@example.com>
2026-05-19 11:04:07 +0530

    Implemented product update inventory API

--- backend/src/main/java/karmpatel/aibilling/controller/ProductController.java
+++ backend/src/main/java/karmpatel/aibilling/controller/ProductController.java
@@ -24,5 +24,20 @@ public class ProductController {
     @PostMapping
     public Product createProduct(@RequestBody Product product) {
         return productRepository.save(product);
+
+
+    }
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product updatedProduct) {
+
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+
+        product.setName(updatedProduct.getName());
+        product.setPrice(updatedProduct.getPrice());
+        product.setQuantity(updatedProduct.getQuantity());
+        product.setCategory(updatedProduct.getCategory());
+
+        return productRepository.save(product);
     }
 }
